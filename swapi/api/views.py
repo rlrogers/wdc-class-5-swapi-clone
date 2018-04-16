e17870ec7b6722c95825a452ffb85af1ca81a4a5
Martin Zugnoni <martin.zugnoni@gmail.com>
2018-04-16 13:44:08 -0300

    Remove solutions

--- swapi/api/views.py
+++ swapi/api/views.py
@@ -35,46 +35,7 @@ def people_list_view(request):
 
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    if request.body:
-        try:
-            payload = json.loads(request.body)
-        except ValueError:
-            return JsonResponse(
-                {"success": False, "msg": "Provide a valid JSON payload"},
-                status=400)
-
-    status = 200
-    if request.method == 'GET':
-        # GET /people
-        qs = People.objects.select_related('homeworld').all()
-        data = [serialize_people_as_json(people) for people in qs]
-    elif request.method == 'POST':
-        # POST /people
-        planet_id = payload.get('homeworld', None)
-        try:
-            planet = Planet.objects.get(id=planet_id)
-        except Planet.DoesNotExist:
-            return JsonResponse(
-                {"success": False, "msg": "Could not find planet with id: {}".format(planet_id)},
-                status=404)
-        try:
-            people = People.objects.create(
-                name=payload['name'],
-                homeworld=planet,
-                height=payload['height'],
-                mass=payload['mass'],
-                hair_color=payload['hair_color'])
-        except (ValueError, KeyError):
-            return JsonResponse(
-                {"success": False, "msg": "Provided payload is not valid"},
-                status=400)
-        data = serialize_people_as_json(people)
-        status = 201
-    else:
-        data = {"success": False, "msg": "Invalid HTTP method"}
-        status=400
-
-    return JsonResponse(data, status=status, safe=False)
+    pass
 
 
 @csrf_exempt
@@ -98,56 +59,4 @@ def people_detail_view(request, people_id):
 
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    try:
-        people = People.objects.get(id=people_id)
-    except People.DoesNotExist:
-        return JsonResponse(
-            {"success": False, "msg": "Could not find people with id: {}".format(people_id)},
-            status=404)
-
-    if request.body:
-        try:
-            payload = json.loads(request.body)
-        except ValueError:
-            return JsonResponse(
-                {"success": False, "msg": "Provide a valid JSON payload"},
-                status=400)
-
-    status = 200
-    if request.method == 'GET':
-        # GET /people/:id
-        data = serialize_people_as_json(people)
-    elif request.method in ['PUT', 'PATCH']:
-        # PUT/PATCH /people/:id
-        for field in ['name', 'homeworld', 'height', 'mass', 'hair_color']:
-            if not field in payload:
-                if request.method == 'PATCH':
-                    continue
-                return JsonResponse(
-                    {"success": False, "msg": "Missing field in full update"},
-                    status=400)
-
-            if field == 'homeworld':
-                try:
-                    payload['homeworld'] = Planet.objects.get(id=payload['homeworld'])
-                except Planet.DoesNotExist:
-                    return JsonResponse(
-                        {"success": False, "msg": "Could not find planet with id: {}".format(payload['homeworld'])},
-                        status=404)
-            try:
-                setattr(people, field, payload[field])
-                people.save()
-            except ValueError:
-                return JsonResponse(
-                    {"success": False, "msg": "Provided payload is not valid"},
-                    status=400)
-        data = serialize_people_as_json(people)
-    elif request.method == 'DELETE':
-        # DELETE /people/:id
-        people.delete()
-        data = {"success": True}
-    else:
-        data = {"success": False, "msg": "Invalid HTTP method"}
-        status=400
-
-    return JsonResponse(data, status=status, safe=False)
+    pass
--- swapi/training/views.py
+++ swapi/training/views.py
@@ -10,7 +10,7 @@ def text_response(request):
     Return a HttpResponse with a simple text message.
     Check that the default content type of the response must be "text/html".
     """
-    return HttpResponse('Hello World! This is not a JSON')
+    pass
 
 
 def looks_like_json_response(request):
@@ -18,7 +18,7 @@ def looks_like_json_response(request):
     Return a HttpResponse with a text message containing something that looks
     like a JSON document, but it's just "text/html".
     """
-    return HttpResponse('{"name": "Luke Skywalker"}')
+    pass
 
 
 def simple_json_response(request):
@@ -26,17 +26,14 @@ def simple_json_response(request):
     Return an actual JSON response by setting the `content_type` of the HttpResponse
     object manually.
     """
-    return HttpResponse(
-        '{"name": "Luke Skywalker"}',
-        content_type='application/json')
+    pass
 
 
 def json_response(request):
     """
     Return the same JSON document, but now using a JsonResponse instead.
     """
-    new_dict = {"name": "Luke Skywalker"}
-    return JsonResponse(new_dict)
+    pass
 
 
 def json_list_response(request):
@@ -47,19 +44,14 @@ def json_list_response(request):
     the JsonResponse object it order to avoid built-in validation.
     https://docs.djangoproject.com/en/2.0/ref/request-response/#jsonresponse-objects
     """
-    new_list = [
-        {"name": "Luke Skywalker"},
-        {"name": "R2-D2"},
-    ]
-    return JsonResponse(new_list, safe=False)
+    pass
 
 
 def json_error_response(request):
     """
     Return a JsonResponse with an error message and 400 (Bad Request) status code.
     """
-    new_dict = {"success": False, "error": "Something went wrong"}
-    return JsonResponse(new_dict, status=400)
+    pass
 
 
 @csrf_exempt
@@ -69,10 +61,7 @@ def only_post_request(request):
     everything is OK, and the status code `200`. If it's a different request
     method, return a `400` response with an error message.
     """
-    if request.method != 'POST':
-        return JsonResponse(
-            {"success": False, "msg": "We only allow POST requests"}, status=400)
-    return JsonResponse({"success": True, "msg": "Yay! You did it."})
+    pass
 
 
 @csrf_exempt
@@ -81,30 +70,14 @@ def post_payload(request):
     Write a view that only accepts POST requests, and processes the JSON
     payload available in `request.body` attribute.
     """
-    if request.method != 'POST':
-        return JsonResponse(
-            {"success": False, "msg": "We only support POST requests"}, status=400)
-
-    try:
-        payload = json.loads(request.body)
-    except ValueError:
-        return JsonResponse(
-            {"success": False, "msg": "Provide a valid JSON payload"}, status=400)
-
-    if not payload:
-        msg = "We got no payload :-("
-    else:
-        msg = "Yay! We got your payload: {}".format(dict(payload))
-    return JsonResponse({"success": True, "msg": msg})
+    pass
 
 
 def custom_headers(request):
     """
     Return a JsonResponse and add a custom header to it.
     """
-    response = JsonResponse({"success": True})
-    response['X-RMOTR-IS-AWESOME'] = True
-    return response
+    pass
 
 
 def url_int_argument(request, first_arg):
@@ -112,8 +85,7 @@ def url_int_argument(request, first_arg):
     Write a view that receives one integer parameter in the URL, and displays it
     in the response text.
     """
-    return JsonResponse(
-        {"success": True, "msg": "We received this argument: {}".format(first_arg)})
+    pass
 
 
 def url_str_argument(request, first_arg):
@@ -121,8 +93,7 @@ def url_str_argument(request, first_arg):
     Write a view that receives one string parameter in the URL, and displays it
     in the response text.
     """
-    return JsonResponse(
-        {"success": True, "msg": "We received this argument: {}".format(first_arg)})
+    pass
 
 
 def url_multi_arguments(request, first_arg, second_arg):
@@ -130,8 +101,7 @@ def url_multi_arguments(request, first_arg, second_arg):
     Write a view that receives two parameters in the URL, and display them
     in the response text.
     """
-    return JsonResponse(
-        {"success": True, "msg": "We received this two arguments: {}, {}".format(first_arg, second_arg)})
+    pass
 
 
 def get_params(request):
@@ -139,9 +109,4 @@ def get_params(request):
     Write a view that receives GET arguments and display them in the
     response text.
     """
-    arguments = request.GET
-    if not arguments:
-        msg = "We got no arguments :-("
-    else:
-        msg = "Yay! We got your arguments: {}".format(dict(arguments))
-    return JsonResponse({"success": True, "msg": msg})
+    pass
